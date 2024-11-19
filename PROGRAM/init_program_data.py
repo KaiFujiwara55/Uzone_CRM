2ef22d623556a1b642543fa95b0e25c53c71e996
KaiFujiwara55 <fkaiku42504261@outlook.com>
2024-11-19 14:41:32 +0900

    特定のシートを参照できるようにプログラムを拡張

--- PROGRAM/init_program_data.py
+++ PROGRAM/init_program_data.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 load_dotenv("MASTER_DATA/.env")
 import datetime
@@ -35,36 +36,53 @@ def get_spread_sheet(spreadsheet_key):
 
     return spreadsheet
 
-def spreadsheet_to_df(spreadsheet, sheet_name):
-    df = pd.DataFrame(spreadsheet.worksheet(sheet_name))
+def spreadsheet_to_df(spreadsheet_key, sheet_name):
+    spreadsheet = get_spread_sheet(spreadsheet_key)
+    df = pd.DataFrame(spreadsheet.worksheet(sheet_name).get_all_values())
     df.columns = df.iloc[0]
     df = df.drop(0, axis=0)
     return df
 
+def get_new_information(df, data_columns, flg_column):
+    df = df.dropna(subset=flg_column)
+    df = df[data_columns]
+
+    return df
+
 def make_mail_status_csv(year_month_date):
-    spreadsheet = get_spread_sheet(os.environ.get("SPRADSHEET_KEY"))
-    df = spreadsheet_to_df(spreadsheet, os.environ.get("SHEET_NAME"))
+    master_df = spreadsheet_to_df(os.environ.get("MASTER_SPREADSHEET_KEY"), os.environ.get("MASTER_SHEET_NAME"))
+    master_df = master_df[str_to_list(os.environ.get("MASTER_USE_COLUMNS"))]
+    master_df.columns = ["account_name", "mail_address"]
 
-    df = df[str_to_list(os.environ.get("USE_COLUMNS"))]
-    df.columns = ["account_name", "mail_address"]
+    # 別で読み取るデータ
+    check_spreadsheet_dic = json.loads(os.environ.get("CHECK_SPREADSHEET_DIC"))
+    for spreadsheet_key, use_info_dic in check_spreadsheet_dic.items():
+        for use_sheet, use_columns in use_info_dic.items():
+            check_df = spreadsheet_to_df(spreadsheet_key, use_sheet)
+            check_df = get_new_information(check_df, use_columns[:-1], use_columns[-1])
+            check_df.columns = ["account_name", "mail_address"]
 
-    tmp_df = df[df["mail_address"].str.contains("、")]
-    df = df[~df["mail_address"].str.contains("、")]
+            master_df = pd.concat([master_df, check_df])
+    
+    tmp_df = master_df[master_df["mail_address"].str.contains("、")]
+    master_df = master_df[~master_df["mail_address"].str.contains("、")]
     for idx, row in tmp_df.iterrows():
         mail_address_list = row["mail_address"].split("、")
         for mail_address in mail_address_list:
             new_row = pd.Series([row["account_name"], mail_address], index=["account_name", "mail_address"])
-            df = pd.concat([df, pd.DataFrame([new_row])])
+            master_df = pd.concat([master_df, pd.DataFrame([new_row])])
 
-    df = df.reset_index(drop=True)
-    for idx, row in df.iterrows():
+    master_df = master_df.reset_index(drop=True)
+    for idx, row in master_df.iterrows():
         account_name = row["account_name"]
         if account_name[-1] == "店":
-            df.iloc[idx, 0] = df.iloc[idx, 0][:-1]
+            master_df.iloc[idx, 0] = master_df.iloc[idx, 0][:-1]
 
-    df["is_send"] = "未"
+    master_df = master_df.drop_duplicates()
 
-    df.to_csv(f".//TRAN_DATA//{year_month_date}//mail_status.csv", index=False)
+    master_df["is_send"] = "未"
+
+    master_df.to_csv(f".//TRAN_DATA//{year_month_date}//mail_status.csv", index=False)
 
 if __name__ == "__main__":
     # year-month-dataを作成
