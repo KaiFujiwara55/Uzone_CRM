ae9392706ccf9ec31639bcfcd000ae483107c597
KaiFujiwara55 <fkaiku42504261@outlook.com>
2024-11-19 13:23:38 +0900

    作業前

--- Uzone_CRM_データセット.bat
+++ Uzone_CRM_データセット.bat
@@ -1,8 +1,8 @@
 @echo off
 echo process start!
 
-cd C:\\CRM_PROGRAM\\Uzone_CRM
-C:\\Users\\info\\AppData\\Local\\Programs\\Python\\Python311\\python.exe PROGRAM\\init_program_data.py
+cd C:\Users\info\python_program\Uzone_CRM
+C:\Python312\python.exe PROGRAM\init_program_data.py
 
 echo process compleated!
 
